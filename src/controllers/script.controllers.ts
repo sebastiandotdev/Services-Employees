a2a702ba52befa2085d2c303234046b5b9d93f88
Sebastian009w <ia3266505@gmail.com>
2023-02-19 22:00:57 -0500

    se crea usuario con post relacionando tablas con prisma

--- src/controllers/script.controllers.ts
+++ src/controllers/script.controllers.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import { prisma } from "../connection/script.connect";
 
 export const getEmployees = async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
   res.json(users);
 };
