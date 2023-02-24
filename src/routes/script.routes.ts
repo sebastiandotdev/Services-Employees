dbc4d825adaa16462d7b2e8e2a141e4e7f41010d
Sebastian009w <ia3266505@gmail.com>
2023-02-23 20:05:31 -0500

    delete Employees

--- src/routes/script.routes.ts
+++ src/routes/script.routes.ts
@@ -4,6 +4,7 @@ import {
   saveEmployees,
   employeesById,
   updateEmployees,
+  deleteEmployees,
 } from "../controllers/script.controllers";
 
 const router = Router();
@@ -12,5 +13,6 @@ router.get("/", getEmployees);
 router.get("/:id", employeesById);
 router.post("/", saveEmployees);
 router.put("/:id", updateEmployees);
+router.delete("/:id", deleteEmployees);
 
 export { router };
